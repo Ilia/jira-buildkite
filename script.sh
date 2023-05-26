0dae88266f57e6e905b0ee1532545326b19ad5e7
Ilia Mogilevsky <ilia.mogilevsky@smartbear.com>
2023-05-26 12:18:25 +1000

    chore: adding auth

--- script.sh
+++ script.sh
@@ -6,9 +6,16 @@ JIRA_VERSION_NAME=$1
 # Jira configuration in order to create version and assign tickets that will be released to it.
 JIRA_PROJECT_ID=17612
 JIRA_URL=https://smartbear.atlassian.net
+
+JIRA_AUTH=$(aws ssm get-parameter --name /${ENVIRONMENT}/jira/auth | jq -r .Parameter.Value)
+
 if [ -z ${JIRA_AUTH} ]; then 
   echo "JIRA_AUTH not set, please set it with by exporting JIRA_AUTH=email:token, you can create a token here: https://id.atlassian.com/manage-profile/security/api-tokens"
   exit 128
+fi
+
+if [ -z ${JIRA_VERSION_NAME} ]; then 
+  JIRA_VERSION_NAME=${RELEASE_VERSION}
 fi
 
 echo "Getting Versions" 
